451d53217fa3603567d3c0188cc6f5e2f11af2ca
Brenden <shippbrenden@gmail.com>
2025-01-23 15:29:03 -0600

    Added vertical movement

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -6,18 +6,18 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	var vdirection := Input.get_axis("ui_up", "ui_down")
 	
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if vdirection:
+		velocity.y = vdirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var hdirection := Input.get_axis("ui_left", "ui_right")
+	if hdirection:
+		velocity.x = hdirection * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
