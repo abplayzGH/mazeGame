a0929191240eb4a954b919f7bbbf561d98fbc778
Brenden <shippbrenden@gmail.com>
2025-01-23 15:40:41 -0600

    Did somethign

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 320.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -11,7 +11,7 @@ func _physics_process(_delta: float) -> void:
 	if vdirection:
 		velocity.y = vdirection * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, 50)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -19,6 +19,6 @@ func _physics_process(_delta: float) -> void:
 	if hdirection:
 		velocity.x = hdirection * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 50)
 
 	move_and_slide()
