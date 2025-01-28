d931ce0828a6322a2311d68b4b1e5a1307c12106
Adam Lawrence-Holzschuher <adam@impostertwo.com>
2025-01-28 10:48:54 -0600

    srge

--- scenes/tile_map.gd
+++ scenes/tile_map.gd
@@ -4,13 +4,10 @@ extends TileMap
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	
+	mazeGenerate()
 	var WINDOW_SIZE = window.size
 	var tile_count = WINDOW_SIZE / 32
-	print(WINDOW_SIZE)
-	print(tile_count)
 	for x in tile_count[0]:
-		print(x)
 		for y in tile_count[1]:
 			set_cell(0, Vector2i(x, y), 0, Vector2i(6,5))
 			
@@ -19,3 +16,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func mazeGenerate() -> void:
+	var rng = RandomNumberGenerator.new()
+	print(window.size/ 32)
+	#print(rng.randi_range())
