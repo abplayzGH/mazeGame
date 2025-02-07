b3fac0ca25e8b7117525ea078a50a7302a7c3aca
Adam Lawrence <adam@impostertwo.com>
2025-02-06 18:34:31 -0600

    no kill tilemap

--- scripts/movement4.gd
+++ scripts/movement4.gd
@@ -9,7 +9,7 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(_delta: float) -> void:
-	var vdirection := Input.get_axis("up3", "down3")
+	var vdirection := Input.get_axis("up4", "down4")
 	
 	if vdirection:
 		velocity.y = vdirection * SPEED
@@ -18,7 +18,7 @@ func _physics_process(_delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var hdirection := Input.get_axis("left3", "right3")
+	var hdirection := Input.get_axis("left4", "right4")
 	if hdirection:
 		velocity.x = hdirection * SPEED
 	else:
@@ -39,7 +39,7 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name != "Player4":
+	if body.name != "Player4" and body.name != "TileMap":
 		print("You died") 
 		print(body)
 		body.position = Vector2(-1000,-1000)
